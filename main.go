2d0efc2bef214f13c7503f7980f0f6aa3c7adc97
Zeng, Rita <rita.zeng@acer.com>
2023-06-13 10:35:06 +0800

    feat: add multiple web page

--- main.go
+++ main.go
@@ -5,16 +5,32 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "Hello, world!")
+
+	if err != nil {
+		fmt.Println("Error occur: ", err)
+	}
+
+	fmt.Printf("Number of bytes written: %d", n)
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "About page")
+
+	if err != nil {
+		fmt.Println("Error occur: ", err)
+	}
+
+	fmt.Printf("Number of bytes written: %d", n)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println("Error occur:", err)
-		}
-
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Printf("Server port number: %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
